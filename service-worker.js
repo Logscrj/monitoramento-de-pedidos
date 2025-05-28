cd92f8f9dcc6d5b4fb6e5d98ef15e3f7af0bfa28
Logscrj <logisticascrj@gmail.com>
2025-05-28 06:19:06 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+
+const CACHE_NAME = 'monitoramento-cache-v1';
+const urlsToCache = ['./', './index.html', './dashboard.html', './rastreamento.html', './manifest.json'];
+self.addEventListener('install', event => {
+ event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+self.addEventListener('fetch', event => {
+ event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
